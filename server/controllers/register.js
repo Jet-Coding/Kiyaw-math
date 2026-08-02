8dc185bf597d5857625fbc0992f40da9a48cdafe
Jet-Coding <sindoljerald@gmail.com>
2026-08-02 08:15:57 +0800

    updating the server side of this webapp.

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,1 +1,6 @@
- 
+const db = require("../routes/db-config");
+const bcrypt = require("bcrypt.js");
+
+const register = async (req, res) => {
+	const { name, email, password, passwordConfirm } = req.body;
+};
